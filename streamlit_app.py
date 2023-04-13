3ddd1c1342e1b0b815c03b54f652a7fe8264c347
mpqlearning <130701769+mpqlearning@users.noreply.github.com>
2023-04-13 13:34:28 -0700

    Update streamlit_app.py
    added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title("My Parents new healthy diner")
 streamlit.header("Breakfast menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach, and Rocket Smoothie")
+streamlit.text("Hard-Boiled Free range Egg")
