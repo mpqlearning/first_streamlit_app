133a4c2c10f270900972f2ca16d302d675c6a05c
mpqlearning <130701769+mpqlearning@users.noreply.github.com>
2023-04-13 13:36:00 -0700

    Update streamlit_app.py
    emoji + toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title("My Parents new healthy diner")
 streamlit.header("Breakfast menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach, and Rocket Smoothie")
-streamlit.text("Hard-Boiled Free range Egg")
+streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗Kale, Spinach, and Rocket Smoothie")
+streamlit.text("🐔Hard-Boiled Free range Egg")
+streamlit.text("🥑Avocado Toast")
