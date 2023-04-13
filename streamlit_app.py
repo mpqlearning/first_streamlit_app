76fc7a1766fc477bc41e6a437a5eb25a8daf74b0
mpqlearning <130701769+mpqlearning@users.noreply.github.com>
2023-04-13 13:36:51 -0700

    Update streamlit_app.py
    smoothie option

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title("My Parents new healthy diner")
 streamlit.header("Breakfast menu")
-streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
-streamlit.text("🥗Kale, Spinach, and Rocket Smoothie")
-streamlit.text("🐔Hard-Boiled Free range Egg")
-streamlit.text("🥑Avocado Toast")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach, and Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
