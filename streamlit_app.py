00528942f983a7726b636f5720d444fc9039e188
mpqlearning <130701769+mpqlearning@users.noreply.github.com>
2023-04-13 13:40:03 -0700

    Update streamlit_app.py
    added fruit menu data farme

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
 
+
+
 streamlit.title("My Parents new healthy diner")
 streamlit.header("Breakfast menu")
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
@@ -9,3 +11,5 @@ streamlit.text("🐔 Hard-Boiled Free range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
