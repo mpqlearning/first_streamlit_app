306aa6b48fda19f1c97c935470138fd46b638924
mpqlearning <130701769+mpqlearning@users.noreply.github.com>
2023-04-17 12:39:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,13 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
+#snowflake function
+def get_fruit_load_list():
+   with my_cur = my_cnx.cursor() as my_cur;
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
+   
+
 
 streamlit.title("My Parents new healthy diner")
 streamlit.header("Breakfast menu")
@@ -43,16 +50,18 @@ except URLError as e:
     streamlit.error()   
 
 streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Add button to load fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
 #allow user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')");
 
