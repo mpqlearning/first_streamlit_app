69d7f56753b3936e94050042b0dface9a02fe15f
mpqlearning <130701769+mpqlearning@users.noreply.github.com>
2023-04-17 12:40:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ def get_fruityvice_data(this_fruit_choice):
 
 #snowflake function
 def get_fruit_load_list():
-   with my_cnx.cursor() as my_cur;
+   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
     return my_cur.fetchall()
    
