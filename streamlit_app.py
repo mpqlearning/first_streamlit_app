82403cfeef64b6a4998aaa3d6e50f1d65d040672
mpqlearning <130701769+mpqlearning@users.noreply.github.com>
2023-04-17 13:14:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,6 +66,7 @@ if streamlit.button('Get Fruit Load List'):
 #allow user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 insert_row_snowflake(add_my_fruit)
+
 #streamlit.write('Thanks for adding ', add_my_fruit)
 
 
