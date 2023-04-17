a52c939f5dd855614828acc45b730613de933f99
mpqlearning <130701769+mpqlearning@users.noreply.github.com>
2023-04-17 12:43:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ try:
 except URLError as e:
     streamlit.error()   
 
-#streamlit.stop()
+
 
 streamlit.header("The fruit load list contains:")
 #Add button to load fruit
@@ -58,6 +58,7 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
+streamlit.stop()
 #allow user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
